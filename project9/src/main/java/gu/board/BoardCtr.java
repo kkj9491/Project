04223e0d9654b7b132527bd26e62eac0d51ab80e
kkj <kkj@LAPTOP-Q76N1JH7>
2018-12-19 19:20:04 +0900

    18.12.19 update

--- project9/src/main/java/gu/board/BoardCtr.java
+++ project9/src/main/java/gu/board/BoardCtr.java
@@ -1,8 +1,10 @@
 package gu.board;
 
+import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,6 +16,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import gu.admin.board.BoardGroupSvc;
 import gu.admin.board.BoardGroupVO;
 import gu.common.Field3VO;
+import gu.common.FileUtil;
+import gu.common.FileVO;
+import gu.common.TreeMaker;
+import gu.common.UtilEtc;
 import gu.etc.EtcSvc;
 
 @Controller
@@ -114,9 +120,136 @@ public class BoardCtr {
 			}
 		}
 		
+		String[] fileno = request.getParameterValues("fileno");
+		FileUtil fs = new FileUtil();
+		List<FileVO> filelist = fs.saveAllFiles(boardInfo.getUploadfile());
 		
+		boardSvc.insertBoard(boardInfo, filelist, fileno);		
 		
-		return null;
+		return "redirect:/boardList?bgno=" + boardInfo.getBgno();
 	}
+	
+	
+	//글 읽기
+	@RequestMapping(value = "/boardRead")
+	public String boardRead(HttpServletRequest request, ModelMap modelMap) {
+		String userno = request.getSession().getAttribute("userno").toString();
+		
+		Integer alertcount = etcSvc.selectAlertCount(userno);
+		modelMap.addAttribute("alertcount", alertcount);
+		
+		String bgno = request.getParameter("bgno");
+		String brdno = request.getParameter("brdno");
+		
+		Field3VO f3 = new Field3VO(brdno, userno, null);
+		
+		boardSvc.updateBoardRead(f3);
+		BoardVO boardInfo = boardSvc.selectBoardOne(f3);
+		List<?> listview = boardSvc.selectBoardFileList(brdno);
+		List<?> replylist = boardSvc.selectBoardReplyList(brdno);
+		
+		BoardGroupVO bgInfo = boardSvc.selectBoardGroupOne4Used(boardInfo.getBgno());
+		if(bgInfo == null) {
+			return "board/BoardGroupFail";
+		}
+		
+		modelMap.addAttribute("boardInfo", boardInfo);
+		modelMap.addAttribute("listview", listview);
+		modelMap.addAttribute("replylist", replylist);
+		modelMap.addAttribute("bgno", bgno);
+		modelMap.addAttribute("bgInfo", bgInfo);
+				
+		return "board/BoardRead";
+	}
+	
+	
+	//글 삭제
+	@RequestMapping(value = "/boardDelete")
+	public String boardDelete(HttpServletRequest request) {
+		String brdno = request.getParameter("brdno");
+		String bgno = request.getParameter("bgno");
+		String userno = request.getSession().getAttribute("userno").toString();
+		
+		BoardVO boardInfo = new BoardVO();
+		boardInfo.setBrdno(brdno);
+		boardInfo.setUserno(userno);
+		String chk = boardSvc.selectBoardAuthChk(boardInfo);
+		if(chk == null) {
+			return "common/noAuth";
+		}
+		
+		boardSvc.deleteBoardOne(brdno);
+		
+		return "redirect:/boardList?bgno=" + bgno;
+	}
+	
+	
+	//게시판 트리. Ajax용
+	@RequestMapping(value = "/boardListByAjax")
+	public void boardListByAjax(HttpServletResponse response, ModelMap modelMap) {
+		List<?> listview = boardGroupSvc.selectBoardGroupList();
+		
+		TreeMaker tm = new TreeMaker();
+		String treeStr = tm.makeTreeByHierarchy(listview);
+		
+		response.setContentType("application/json;charset=UTF-8");
+		try {
+			response.getWriter().print(treeStr);
+		} catch (IOException ex) {
+			LOGGER.error("boardListByAjax");
+		}
+		
+	}
+	
+	
+	
+	/*=========================================================
+	     좋아요 저장.
+	*/
+	@RequestMapping(value = "/addBoardLike")
+	public void addBoardLike(HttpServletRequest request, HttpServletResponse response) {
+		String brdno = request.getParameter("brdno");
+		String userno = request.getSession().getAttribute("userno").toString();
+		
+		boardSvc.insertBoardLike(new Field3VO(brdno, userno, null));		
+	
+		UtilEtc.responseJsonValue(response, "OK");
+	}
+	
+	
+	/*========================================================
+			
+	댓글 저장.
+	*/
+	@RequestMapping(value = "/boardReplySave")
+	public String boardReplySave(HttpServletRequest request, HttpServletResponse response, BoardReplyVO boardReplyInfo, ModelMap modelMap) {
+		String userno = request.getSession().getAttribute("userno").toString();
+		boardReplyInfo.setUserno(userno);
+		
+		if(boardReplyInfo.getReno() != null && !"".equals(boardReplyInfo.getReno())) {
+			String chk = boardSvc.selectBoardReplyAuthChk(boardReplyInfo);
+			if(chk == null) {
+				UtilEtc.responseJsonValue(response, "");
+				return null;
+			}
+		}
+		
+		boardReplyInfo = boardSvc.insertBoardReply(boardReplyInfo);		
+		//boardReplyInfo.setRewriter(request.getSession().getAttribute("usernm").toString());
+		
+		modelMap.addAttribute("replyInfo", boardReplyInfo);
+		
+		return "board/BoardReadAjax4Reply";		
+	}
+	
+	
+	// 댓글 삭제
+	@RequestMapping(value = "/boardReplyDelete")
+	public void boardReplyDelete(HttpServletRequest request, HttpServletResponse response, BoardReplyVO boardReplyInfo) {
+		String userno = request.getSession().getAttribute("userno").toString();
+		
+		
+	}
+	
 	
 }
